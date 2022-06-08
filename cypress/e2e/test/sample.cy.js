e2f95ede97a2e79c6768cdae6e10a1ca169c5604
mrunalp22 <37875935+mrunalp22@users.noreply.github.com>
2022-06-08 17:15:08 +0530

    Added cypress and sample test file

---
+++ cypress/e2e/test/sample.cy.js
@@ -0,0 +1,13 @@
+context('Actions', () => {
+    beforeEach(() => {
+        cy.visit("https://sbi.bnpl-demo.com/")
+
+})
+
+describe('loginTest',  () => {
+    it('Visits the admin link', () => {
+        cy.visit("https://sbi.bnpl-demo.com/")
+     })
+})
+
+})
